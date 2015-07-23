47e31044315a80ba2d82eeeb6ba4b224e6fbcfde
Sakura <ssschensss@126.com>
2015-07-23 11:13:06 +0800

    Initial Commit

---
+++ SKModel/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SKModel
+//
+//  Created by 陈建辉 on 15/7/23.
+//  Copyright (c) 2015年 Sakura. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SKModel/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  SKModel
+//
+//  Created by 陈建辉 on 15/7/23.
+//  Copyright (c) 2015年 Sakura. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
